75076a32328b1e2ae10938ec560d1c7d083727c2
karadalex <karadalex@gmail.com>
2019-03-07 18:04:21 +0200

    python web gui (TODO with Flask)

--- pyqt-gui/src/app.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PySide2.QtWidgets import QApplication, QLabel
-
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    label = QLabel("Hello World")
-    label.show()
-    sys.exit(app.exec_())
