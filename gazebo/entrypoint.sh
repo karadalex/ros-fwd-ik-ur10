971fc82a2ef05f7a525649eae859d66839e21d90
karadalex <karadalex@gmail.com>
2019-03-11 15:32:16 +0200

    successfully load UR10 robot in Gazebo server and display it in Gazebo Web client simulation

--- gazebo/entrypoint.sh
+++ gazebo/entrypoint.sh
@@ -2,12 +2,17 @@
 
 # setup gazebo environment
 source "/usr/share/gazebo/setup.sh"
-gzserver --verbose & npm start & 
+# gzserver --verbose & npm start &
+npm start &
 
 source /opt/ros/kinetic/setup.bash
 mkdir -p /ros_ws/src
 cd /ros_ws
 catkin_make
 source devel/setup.bash
-# run gzserver and gzweb
+
+# Copy volume containing meshes in gazebo web server assets folder
+cp -r /ros_ws/src/ur_description ~/gzweb/http/client/assets
+
+# Entrypoint for command
 exec "$@"
