6f9866531932008e9d7d0b4eb6f144c4a81beee3
karadalex <karadalex@gmail.com>
2019-04-03 22:05:34 +0300

    solve forward kinematics symbolically first with sympy

--- kinematics_py/src/fwd_kinematics.py
+++ kinematics_py/src/fwd_kinematics.py
@@ -2,28 +2,31 @@
 import numpy.matlib
 import numpy as np
 from math import pi, sin, cos
+from sympy import *
 import rospy
 from trajectory_msgs.msg import JointTrajectory
+
 
 # Takes DH parameters and joint variables as input
 # and returns transformation matrix of forward kinematics
 def forward(theta, L, d, a):
     # List of transformation matrices of links
     # Each transformation is calculated with respect to previous link
-    M_joints = np.zeros((6, 4, 4))
+    M_joints = []
     for i in range(6):
-        M_joints[i] = [
+        M_joint_i = Matrix([
             [cos(theta[i]), -sin(theta[i]), 0, L[i]],
             [sin(theta[i]) * cos(a[i]), cos(theta[i]) * cos(a[i]), -sin(a[i]), -sin(a[i]) * d[i]],
             [sin(theta[i]) * sin(a[i]), cos(theta[i]) * sin(a[i]), cos(a[i]), cos(a[i]) * d[i]],
             [0, 0, 0, 1]
-        ]
+        ])
+        M_joints.append(M_joint_i)
 
     # Transformation matrix of last join (Coordinate system {6})
     # with respect to Universal coordinate system {0}
-    M = np.matlib.identity(4)
+    M = eye(4)
     for i in range(6):
-        M = np.matmul(M, M_joints[i])
+        M = M * M_joints[i]
 
     return M
 
@@ -41,24 +44,24 @@ if __name__ == "__main__":
 
     # Get DH Parameters from ROS Parameter server or use defaults
     # if parameters don't exist in parameter server
-    L = rospy.get_param('/dh_params/L', [0, -0.612, -0.5723, 0, 0, 0])
-    d = rospy.get_param('/dh_params/d', [0.1273, 0, 0, 0.163941, 0.1157, 0.0922])
-    a = rospy.get_param('/dh_params/a', [pi/2, 0, 0, pi/2, -pi/2, 0])
+    L = rospy.get_param('/dh_params/L', [0, 0.612, 0.572, 0, 0, 0])
+    d = rospy.get_param('/dh_params/d', [0.128, 0, 0, 0.164, 0.116, 0.092])
+    a = rospy.get_param('/dh_params/a', [0, -pi/2, 0, 0, pi/2, -pi/2])
 
     # Joint angles variables
-    # theta = np.zeros(6)
-    theta = [pi/4, pi/4, pi/2, pi/2, pi/4, 0]
+    theta = symbols('th1:7')
 
     M = forward(theta, L, d, a)
+    pprint(M)
+    # Test angles
+    angles = [pi/4, pi/4, pi/2, pi/2, pi/4, 0]
+    M_test = M.subs({ 'th'+str(i+1):angles[i] for i in range(6) })
+    pprint(M_test)
 
     # Position vector in homogeneous coordinates with respect to coordinate system {6}
     p6 = [0, 0, 0, 1]
-
     # Position vector in homogeneous coordinates with respect to universal coordinate system {0}
-    p0 = np.matmul(M, p6)
-
-    print(M)
-    print(p6)
+    p0 = np.matmul(np.array(M_test).astype(np.float64), p6)
     print(p0)
 
     rospy.spin()
